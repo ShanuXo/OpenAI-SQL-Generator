b2bd1553ad060a93a70c81b5931acea9494e911c
ShanuXo <shanusah8001@gmail.com>
2024-03-23 03:40:30 +0530

    Generate Function

--- Server/api.js
+++ Server/api.js
@@ -1,17 +1,21 @@
-import { Configuration , OpenAIApi} from "openai";
+import { OpenAI } from "openai";
 import dotenv from "dotenv";
 dotenv.config();
 
-const openaiAPIKey= process.env.OPENAI_API
+const openaiAPIKey= process.env.OPENAI_API_KEY
 
 if(!openaiAPIKey){
     console.error("No OPENAI API Key Provided");
     process.exit(1);
 } 
 
-const  configuration = new Configuration({
-    apiKey: openaiAPIKey
+const  configuration = new OpenAI({
+    apiKey:process.env.OPENAI_API_KEY
 })
-const openai = new OpenAIApi(configuration)
+
+const openai = new OpenAI(configuration);
 
 export default openai
+
+
+// https://platform.openai.com/api-keys
--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,33 @@
-var  express = require('express')
-var cors = require('cors')
+import generate from './generate.js'
+
+import express from "express";
+import cors from "cors";
+
 
 const app=express();
 
-app.use(cors());
+app.use(cors(
+    { origin: "*" }
+  ));
+  
+app.use(express.json())
 
 const port  = process.env.PORT ||  3005;
 
 app.get("/", (req, res) => {
     res.send("Hello World");
+})
+
+app.post("/generate", async (req,res) =>{
+    const queryDescription = req.body.queryDescription
+    try{
+        const sqlQuery = await generate(queryDescription);
+        res.json({response : sqlQuery})
+    } catch(error)
+    {
+        console.error(error);
+        res.status(500).send("internal Server Error")
+    }
 })
 
 app.listen(port , ()=>{
